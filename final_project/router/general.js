cb18330bb95e88e9320c6e597b33ff8bac29ed1f
Dami34 <damisi.odumosu03@gmail.com>
2025-01-31 21:42:11 -0500

    Implemented route to get book details by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -47,10 +47,23 @@ public_users.get('/author/:author', function (req, res) {
   });  
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/title/:title', function (req, res) {
+    const title = req.params.title; // Retrieve title from request parameters
+    let booksByTitle = [];
+  
+    // Iterate through the 'books' object and find books matching the title
+    Object.keys(books).forEach((isbn) => {
+      if (books[isbn].title.toLowerCase() === title.toLowerCase()) {
+        booksByTitle.push(books[isbn]);
+      }
+    });
+  
+    if (booksByTitle.length > 0) {
+      return res.status(200).json(booksByTitle); // Return list of books with the given title
+    } else {
+      return res.status(404).json({ message: "No books found with this title" }); // Error if no books match
+    }
+  });  
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
